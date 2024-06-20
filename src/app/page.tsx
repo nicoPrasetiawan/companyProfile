a77256b95f0e5545b9e591e668b61244af07521b
nicoPrasetiawan <nicoprasetiawan28@gmail.com>
2024-06-20 21:47:58 +0700

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,41 +7,24 @@ import ServicesPage from "./our-offerings/page";
 import AboutFoodHub from "./about/page";
 import Testimony from "@/components/testimony/testimony";
 import Footer from "@/components/footer/footer";
-import { client } from "@/utils/contentful";
-import { TypeCompanyProfileFields } from "@/types/contentful";
 
-export async function fetchContent() {
-  try {
-    const data = await client.getEntries<TypeCompanyProfileFields>();
-
-    console.log(data.items);
-    return data?.items;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export default async function Home() {
-  const contents = await fetchContent();
-
+export default function Home() {
   return (
     <>
       <header className={classes.header}>
         <div className={classes.slideshow}>
           <ImageSlideshow />
         </div>
-        {contents?.map((content) => (
-          <div key={content.sys.id}>
-            <div className={classes.hero}>
-              <h1>{content.fields.title}</h1>
-              <p>{content.fields.tagline}</p>
-            </div>
-            <div className={classes.cta}>
-              <Link href="/our-offerings">{content.fields.menu1}</Link>
-              <Link href="/meals">{content.fields.menu2}</Link>
-            </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Cultural Cuisines For Culinary Explorers</h1>
+            <p>Discover and Delight in Worldwide Flavors.</p>
           </div>
-        ))}
+          <div className={classes.cta}>
+            <Link href="/our-offerings">Our Offerings</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
       </header>
       <main>
         <ServicesPage />

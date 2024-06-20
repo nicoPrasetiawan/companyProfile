f76a2361e2c8a70275f843645b849f6494935794
nicoPrasetiawan <nicoprasetiawan28@gmail.com>
2024-06-20 22:45:29 +0700

    add fetchContent in the root page.tsx for learning purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,32 @@ import ServicesPage from "./our-offerings/page";
 import AboutFoodHub from "./about/page";
 import Testimony from "@/components/testimony/testimony";
 import Footer from "@/components/footer/footer";
+import { fetchContent } from "./our-offerings/page";
 
-export default function Home() {
+export default async function Home() {
+  const contents = await fetchContent();
+
   return (
     <>
-      <header className={classes.header}>
-        <div className={classes.slideshow}>
-          <ImageSlideshow />
-        </div>
-        <div>
-          <div className={classes.hero}>
-            <h1>Cultural Cuisines For Culinary Explorers</h1>
-            <p>Discover and Delight in Worldwide Flavors.</p>
+      {/* sebenernya ga perlu fetch contentful, tapi pengen coba aja untuk latihan */}
+      {contents?.map((content) => (
+        <header className={classes.header}>
+          <div className={classes.slideshow}>
+            <ImageSlideshow />
           </div>
-          <div className={classes.cta}>
-            <Link href="/our-offerings">Our Offerings</Link>
-            <Link href="/meals">Explore Meals</Link>
+          <div>
+            <div className={classes.hero}>
+              <h1>{content.fields.title}</h1>
+              <p>{content.fields.tagline}</p>
+            </div>
+            <div className={classes.cta}>
+              <Link href="/our-offerings">{content.fields.menu1}</Link>
+              <Link href="/meals">{content.fields.menu2}</Link>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+      ))}
+
       <main>
         <ServicesPage />
 
